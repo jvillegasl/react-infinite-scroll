f637e9bdf3f7f64f7e4c262572156e0a90fbc16a
jvillegasl <jvillegasl@uni.pe>
2023-03-28 01:07:29 -0500

    feat: near screen hook

---
+++ src/hooks/useNearScreen.ts
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useNearScreen<T extends HTMLElement>(
+    options?: IntersectionObserverInit
+) {
+    const unobserveRef = useRef<(() => void) | null>(null);
+    const x = unobserveRef.current;
+    const thresholdRef = useCallback((node: T) => {
+        if (!node) return;
+
+        const callback: IntersectionObserverCallback = function (entries) {
+            const [entry] = entries;
+            setInView(entry.isIntersecting);
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+
+        if (unobserveRef.current) unobserveRef.current();
+
+        unobserveRef.current = () => observer.unobserve(node);
+
+        observer.observe(node);
+    }, []);
+
+    const [inView, setInView] = useState(false);
+
+    return [thresholdRef, inView] as const;
+}
