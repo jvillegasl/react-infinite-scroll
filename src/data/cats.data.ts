f64d49630138cb3ff2505be075533cdc6c258f1e
jvillegasl <jvillegasl@uni.pe>
2023-03-28 01:06:11 -0500

    feat: mock data

---
+++ src/data/cats.data.ts
@@ -0,0 +1,62 @@
+export const CATS_DATA = [
+    {
+        imgUrl: "https://cataas.com/cat/dEB4Tt9FikHEspmK/says/A%20tomcat%20(male",
+        fact: "A tomcat (male cat) can begin mating when he is between 7 and 10 months old.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/1ANDs65qm2hR9o55/says/The%20biggest%20wildcat",
+        fact: "The biggest wildcat today is the Siberian Tiger. It can be more than 12 feet (3.6 m) long (about the size of a small car) and weigh up to 700 pounds (317 kg).",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/0U4jE41oGuUWThFX/says/Cats%20walk%20on",
+        fact: "Cats walk on their toes.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/e3QQHQQaugOfUBBL/says/According%20to%20Hebrew",
+        fact: "According to Hebrew legend, Noah prayed to God for help protecting all the food he stored on the ark from being eaten by rats. In reply, God made the lion sneeze, and out popped a cat.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/eWpWwlm7VGFhhGRA/says/The%20cat%20has",
+        fact: "The cat has 500 skeletal muscles (humans have 650).",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/Zi3aI7QvX2r1D1Zv/says/The%20world%E2%80%99s%20rarest",
+        fact: "The world’s rarest coffee, Kopi Luwak, comes from Indonesia where a wildcat known as the luwak lives. The cat eats coffee berries and the coffee beans inside pass through the stomach. The beans are harvested from the cat’s dung heaps and then cleaned and roasted. Kopi Luwak sells for about $500 for a 450 g (1 lb) bag.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/55j1CyOfwuTFFdvk/says/The%20cat%20who",
+        fact: "The cat who holds the record for the longest non-fatal fall is Andy. He fell from the 16th floor of an apartment building (about 200 ft/.06 km) and survived.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/FhHzBV5u4O2eXsuL/says/A%20female%20cat",
+        fact: "A female cat can be referred to as a molly or a queen, and a male cat is often labeled as a tom.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/SzBJ8MVCz2giHkPF/says/A%20cat%E2%80%99s%20heart",
+        fact: "A cat’s heart beats nearly twice as fast as a human heart, at 110 to 140 beats a minute.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/Z13X18MO1jhNTkrs/says/The%20tiniest%20cat",
+        fact: "The tiniest cat on record is Mr. Pebbles, a 2-year-old cat that weighed 3 lbs (1.3 k) and was 6.1 inches (15.5 cm) high.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/nuQ8YFb0ro01SGhT/says/The%20first%20cat",
+        fact: "The first cat in space was a French cat named Felicette (a.k.a. “Astrocat”) In 1963, France blasted the cat into outer space. Electrodes implanted in her brains sent neurological signals back to Earth. She survived the trip.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/KJYmrVNOVTK1ePOD/says/Cats%20do%20not",
+        fact: "Cats do not think that they are little people. They think that we are big cats. This influences their behavior in many ways.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/QkOfoBnDmxLH5cVJ/says/The%20cat's%20clavicle,",
+        fact: "The cat's clavicle, or collarbone, does not connect with other bones but is buried in the muscles of the shoulder region. This lack of a functioning collarbone allows them to fit through any opening the size of their head.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/503m3ZwEJrqFFOQV/says/All%20cats%20have",
+        fact: "All cats have three sets of long hairs that are sensitive to pressure - whiskers, eyebrows,and the hairs between their paw pads.",
+    },
+    {
+        imgUrl: "https://cataas.com/cat/U78luyYViSfrdPhq/says/A%20cat%20has",
+        fact: "A cat has approximately 60 to 80 million olfactory cells (a human has between 5 and 20 million).",
+    },
+];
