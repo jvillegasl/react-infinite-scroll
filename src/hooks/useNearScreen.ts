c2bd16533a4c1e7c914148956e2d6df3e2f327ec
jvillegasl <jvillegasl@uni.pe>
2023-03-28 01:13:42 -0500

    feat: near screen hook

--- src/hooks/useNearScreen.ts
+++ src/hooks/useNearScreen.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export function useNearScreen<T extends HTMLElement>(
     options?: IntersectionObserverInit
